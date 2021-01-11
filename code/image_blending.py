79214b38a9c76c2b841ed3c1ccea323960741d11
Zhenyuan <szywind@163.com>
2021-01-11 18:45:16 +0800

    update

--- code/image_blending.py
+++ code/image_blending.py
@@ -10,7 +10,9 @@
 import os
 import cv2
 import glob
+import enum
 import numpy as np
+from tqdm import tqdm
 
 from datetime import datetime
 import matplotlib.pyplot as plt
@@ -19,6 +21,20 @@ import matplotlib.pyplot as plt
 root = os.path.dirname(os.path.dirname(__file__))
 input_dir = os.path.join(root, 'figures')
 output_dir = input_dir
+sub_dir = 'set0'
+
+class BLENDER_TYPE(enum.Enum):
+    """
+    - MAX:
+    - AVG:
+    - FEATHER:
+    - MULTIBAND:
+    """
+
+    MAX = 'max'
+    AVG = 'avg'
+    FEATHER = 'feather'
+    MULTIBAND = 'multiband'
 
 def maximum(img_list):
     return np.max(img_list, axis=0)
@@ -29,15 +45,11 @@ def average(img_list):
     h, w = img_list[0].shape[:2]
     mask = np.zeros((h, w))
 
-    # dst = np.sum(img_list, axis=0)
-    # for i in range(h):
-    #     for j in range(w):
-    #         if mask[i, j] == 0: continue
-    #         dst[i, j, :] /= mask[i, j]
-
     for img in img_list:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        mask_i = gray > 0
+        # cv2.imshow('gray: ', gray)
+        # cv2.waitKey()
+        mask_i = gray > 5
         mask_i = mask_i.astype(np.float)
         # plt.imshow(mask_i * 255)
         # plt.show()
@@ -46,23 +58,22 @@ def average(img_list):
     dst = np.sum(img_list, axis=0)
     print('dst shape: ', dst.shape)
     print('mask shape: ', mask.shape)
-
-    # for i in range(h):
-    #     for j in range(w):
-    #         if mask[i, j] == 0: continue
-    #         dst[i, j, :] /= mask[i, j]
     mask = np.clip(mask, 1, len(img_list))
 
-    plt.imshow(mask * 255)
-    plt.show()
+    # kernel = np.ones((5, 5), np.float32) / 25
+    # mask = cv2.filter2D(mask, -1, kernel)
 
-    dst = dst / mask[..., np.newaxis]
+    # mask = cv2.GaussianBlur(mask, (5, 5), 1, sigmaY=1)
+    mask_3d = mask[..., np.newaxis]
+
+    dst = dst / mask_3d
     dst = dst.astype(np.uint8)
+
     return dst
 
 def sift_blending():
-    left = cv2.imread(os.path.join(input_dir, 'wrap_2D_前广角镜头.jpg')) # 'wrap_2D_车头视角2.jpg'
-    right = cv2.imread(os.path.join(input_dir, 'wrap_2D_车轮视角2_right.jpg'))
+    left = cv2.imread(os.path.join(input_dir, sub_dir, 'wrap_2D_前广角镜头.jpg')) # 'wrap_2D_车头视角2.jpg'
+    right = cv2.imread(os.path.join(input_dir, sub_dir, 'wrap_2D_车轮视角2_right.jpg'))
 
     images = [left, right]
     stitcher = cv2.Stitcher.create()
@@ -75,17 +86,63 @@ def sift_blending():
     else:
         print("Failed to stitch.")
 
+def feather_blending(img_list):
+    if len(img_list) == 0:
+        raise ValueError('Error: Input empty image list!')
+    blender = cv2.detail_FeatherBlender()
+    blender.setSharpness(0.02)
+    h, w = img_list[0].shape[:2]
+    blender.prepare((0, 0, w, h)) # called once at start
+
+    for img in tqdm(img_list, desc='process image ...', ncols=100):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        mask = gray > 5
+        blender.feed(img.astype(np.int16), mask.astype(np.uint8), (0,0))
+
+    res, res_mask = blender.blend(None, None)
+    return res.astype(np.uint8)
+
+def multiband_blending(img_list):
+    if len(img_list) == 0:
+        raise ValueError('Error: Input empty image list!')
+    blender = cv2.detail_MultiBandBlender()
+    blender.setNumBands(4)
+    h, w = img_list[0].shape[:2]
+    blender.prepare((0, 0, w, h)) # called once at start
+
+    for img in tqdm(img_list, desc='process image ...', ncols=100):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        mask = gray > 5
+        blender.feed(img.astype(np.int16), mask.astype(np.uint8), (0,0))
+
+    res, res_mask = blender.blend(None, None)
+    return res.astype(np.uint8)
+
+def run(method, img_list):
+    if method == BLENDER_TYPE.MAX:
+        res_img = maximum(img_list)
+    elif method == BLENDER_TYPE.AVG:
+        res_img = average(img_list)
+    elif method == BLENDER_TYPE.FEATHER:
+        res_img = feather_blending(img_list)
+    elif method == BLENDER_TYPE.MULTIBAND:
+        res_img = multiband_blending(img_list)
+    else:
+        raise ValueError('Unsupported blending moethod.')
+    cv2.imshow('result', res_img)
+    # cv2.imwrite(os.path.join(output_dir, 'combined_{}_{}_{}.jpg'.format(sub_dir, method.value, datetime.now().strftime("%d-%m-%Y %H-%M-%S"))), res_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 if __name__ == '__main__':
-    img_path_list = glob.glob(os.path.join(input_dir, 'wrap_2D_*.jpg'))
+    img_path_list = glob.glob(os.path.join(input_dir, sub_dir, 'wrap_2D_*.jpg'))
     img_list = [cv2.imread(img_path) for img_path in img_path_list if os.path.exists(img_path)]
     for img in img_list:
         print(img.shape)
-    res_img = maximum(img_list)
 
-    cv2.imshow('result', res_img)
-    cv2.imwrite(os.path.join(output_dir, 'combined_avg_{}.jpg'.format(datetime.now().strftime("%d-%m-%Y %H-%M-%S"))),
-                res_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # run(BLENDER_TYPE.MAX, img_list)
+    # run(BLENDER_TYPE.AVG, img_list)
+    run(BLENDER_TYPE.MULTIBAND, img_list)
+    run(BLENDER_TYPE.FEATHER, img_list)
 
     # sift_blending()
